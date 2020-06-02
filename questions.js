5794fa67472afbfe624ac706a61d2a7ffbe96a12
Ernst Pierre <emmp318@gmail.com>
2020-06-01 19:59:03 -0500

    Code-Quiz html,css,js and ReadMe Uplaod

---
+++ questions.js
@@ -0,0 +1,39 @@
+var questions = [
+    {
+        title: "What year was the Mario Bros. arcade game released?",
+        choices: ["1983", "1988", "1990", "1991"],
+        answer: "1988"
+    },
+
+    {
+        title: "Who created Mario Bros?",
+        choices: ["Toru Iwatani", "Bill Gates", "Shigeru Miyamoto", "Yukio Kaneoka"],
+        answer: "Shigeru Miyamoto"
+    },
+
+    {
+        title: "What was the inspiration for Pac-Man's Iconic shape?",
+        choices: ["Basketball", "Pizza", "Moon", "Sun"],
+        answer: "Pizza"
+    },
+
+    {
+        title: "Who created Donkey Kong?",
+        choices: ["Shigeru Miyamoto", "Yu Suzuki", "Satoro Iwata", "Hideo Kojima"],
+        answer: "Shigeru Miyamoto"
+    },
+
+    {
+        title: "Which game developer created Contra?",
+        choices: ["Konami", "Namco", "Nintendo", "Capcom"],
+        answer: "Konami"
+    },
+
+    {
+        title: "What is Sonic the headgehog's middle name?",
+        choices: ["John", "Blu", "Maurice", "Spike"],
+        answer: "Maurice"
+    },
+
+
+];
